cfb70fd689f543c1daf89ae9a26c950a515f0967
Lucas Ferrari Lopes <lf.ferrarilucas@gmail.com>
2024-10-15 17:36:10 +0000

    algumas alteraçoes

--- projeto/src/app/app-routing.module.ts
+++ projeto/src/app/app-routing.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { LoginPageComponent } from './login-page/login-page.component';
+import { DashboardPageComponent } from './dashboard-page/dashboard-page.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: "", redirectTo: "dashboard", pathMatch: "full"},
+  {path: "login", component: LoginPageComponent},
+  {path: "dashboard", component: DashboardPageComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- projeto/src/app/app.module.ts
+++ projeto/src/app/app.module.ts
@@ -3,14 +3,22 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app/app.component';
+import { LoginPageComponent } from './login-page/login-page.component';
+import { DashboardPageComponent } from './dashboard-page/dashboard-page.component';
+import { ToolbarComponent } from '../shared-components/toolbar/toolbar.component';
+import { SharedComponentsModule } from '../shared-components/shared-components.module';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginPageComponent,
+    DashboardPageComponent,
+    ToolbarComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    SharedComponentsModule
   ],
   providers: [],
   bootstrap: [
